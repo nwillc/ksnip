604c092f071ab330ccc700caf5a3e3654e0b1324
Nicholas Christopher <nwillc@gmail.com>
2018-04-29 10:40:48 -0400

    working with fxml

--- src/main/kotlin/com/github/nwillc/ksnip/app/SnippetsApp.kt
+++ src/main/kotlin/com/github/nwillc/ksnip/app/SnippetsApp.kt
@@ -6,17 +6,11 @@
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-package com.github.nwillc.ksnip
+package com.github.nwillc.ksnip.app
 
-import javafx.scene.control.Label
-import javafx.scene.layout.HBox
-import tornadofx.App
-import tornadofx.View
+import com.github.nwillc.ksnip.view.SnippetsView
+import tornadofx.*
 
-class HelloWorld : View() {
-    override val root = HBox(Label("Hello world!"))
-}
-
-class HelloWorldApp : App() {
-    override val primaryView = HelloWorld::class
+class SnippetsApp : App() {
+    override val primaryView = SnippetsView::class
 }
--- src/main/kotlin/com/github/nwillc/ksnip/view/SnippetsView.kt
+++ src/main/kotlin/com/github/nwillc/ksnip/view/SnippetsView.kt
@@ -6,22 +6,26 @@
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-package com.github.nwillc.ksnip
+package com.github.nwillc.ksnip.view
 
-import tornadofx.Stylesheet
-import tornadofx.box
-import tornadofx.cssclass
-import tornadofx.px
+import javafx.scene.control.*
+import tornadofx.*
 
-class Styles : Stylesheet() {
-    companion object {
-        val wrapper by cssclass()
-    }
+class SnippetsView : View() {
+    override val root: TabPane by fxml("/views/Snippets.fxml")
+    val categories: ListView<String> by fxid()
+    val titles: ListView<String> by fxid()
+    val text: TextArea by fxid()
+    val categoryName: TextField by fxid()
 
     init {
-        s(wrapper) {
-            padding = box(10.px)
-            spacing = 10.px
-        }
+        categories.items.add("One")
+        titles.items.addAll("hello", "world")
+        text.text = "hello world"
+    }
+
+    fun saveCategory() {
+        categories.items.add(categoryName.text)
     }
 }
+
