63dfafe1352f88474d3df315c8c06721640a06ad
Nicholas Christopher <nwillc@gmail.com>
2019-02-03 19:02:28 -0500

    Cleaning

--- src/main/kotlin/com/github/nwillc/ksnip/controller/PreferencesController.kt
+++ src/main/kotlin/com/github/nwillc/ksnip/controller/PreferencesController.kt
@@ -1,5 +1,16 @@
 /*
- * Copyright (c) 2018, ADP Inc. All Rights Reserved. This property belongs to ADP Inc. Copying in any form is prohibited.
+ * Copyright 2019 nwillc@gmail.com
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any purpose with or
+ * without fee is hereby granted, provided that the above copyright notice and this
+ * permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT,
+ * OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,
+ * DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+ * ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
 package com.github.nwillc.ksnip.controller
@@ -17,15 +28,15 @@ import java.io.File
  */
 class PreferencesController : Controller() {
     companion object {
-        private val PREF_FILE = File(System.getProperty("user.home"), ".snippets.json")
+        private val prefFile = File(System.getProperty("user.home"), ".snippets.json")
     }
 
     private val mapper = ObjectMapper().registerModule(KotlinModule())
     var preferences = Preferences()
 
     init {
-        if (PREF_FILE.canRead()) {
-            preferences = mapper.readValue<Preferences>(PREF_FILE, Preferences::class.java)
+        if (prefFile.canRead()) {
+            preferences = mapper.readValue<Preferences>(prefFile, Preferences::class.java)
         }
     }
 
@@ -33,9 +44,9 @@ class PreferencesController : Controller() {
      * Save the application preferences.
      */
     fun savePreferences() {
-        PREF_FILE.delete()
-        PREF_FILE.createNewFile()
-        val outputStream = PREF_FILE.outputStream()
+        prefFile.delete()
+        prefFile.createNewFile()
+        val outputStream = prefFile.outputStream()
         mapper.writeValue(outputStream, preferences)
     }
 }
